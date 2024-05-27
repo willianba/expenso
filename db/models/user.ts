3e8a96f8f25047166a5f61ab7b4792259a1aa3b9
Willian Alves <wba.alves@gmail.com>
2024-05-27 00:37:03 -0300

    rename models | update user logic

---
+++ db/models/user.ts
@@ -0,0 +1,49 @@
+import { kv } from "@/db/kv.ts";
+import { monotonicUlid } from "@std/ulid";
+
+export type User = {
+  id: string; // ULID
+  name: string;
+  email: string;
+};
+
+type CreateUserInput = Omit<User, "id">;
+
+export default class UserService {
+  public static async create(user: CreateUserInput) {
+    const userId = monotonicUlid();
+    const userWithId = { ...user, id: userId };
+
+    const key = ["user", user.email];
+    const createRes = await kv
+      .atomic()
+      .check({ key, versionstamp: null })
+      .set(key, userWithId)
+      .commit();
+
+    if (!createRes.ok) {
+      throw new Deno.errors.AlreadyExists("User already exists");
+    }
+
+    return userWithId as User;
+  }
+
+  public static async getByEmail(email: string) {
+    const userRes = await kv.get<User>(["user", email]);
+    return userRes.value;
+  }
+
+  public static async getBySessionId(sessionId: string) {
+    const key = ["user_session", sessionId];
+    const eventualUser = await kv.get<User>(key, {
+      consistency: "eventual",
+    });
+
+    if (eventualUser.value !== null) {
+      return eventualUser.value;
+    }
+
+    const res = await kv.get<User>(key);
+    return res.value;
+  }
+}
