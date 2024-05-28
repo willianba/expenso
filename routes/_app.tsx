8019ea68a3b2ad762b17133a16210c0852ff927c
Willian Alves <wba.alves@gmail.com>
2024-05-27 23:19:06 -0300

    add normalize

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>wexter</title>
+        <link rel="stylesheet" href="/normalize.css" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
