ab21681c4befff532d65fa5de1003e8aa0fc64a3
Willian Alves <wba.alves@gmail.com>
2024-06-01 16:48:14 -0300

    update main page and add a mock table

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,13 +1,37 @@
+import { ComponentChildren } from "preact";
+
 type CardProps = {
-  height?: string;
+  children: ComponentChildren;
+  title: string;
+  classes?: string;
 };
 
 export default function Card(props: CardProps) {
-  const { height } = props;
+  const { classes, children, title } = props;
 
   return (
-    <div class={`card w-full bg-base-100 shadow-s ${height}`}>
-      <div class="card-body items-center text-center">
+    <div
+      class={`card card-compact w-full bg-base-100 shadow-s ${classes}`}
+    >
+      <div class="card-body h-full">
+        <div class="flex justify-between">
+          <h2 class="card-title">{title}</h2>
+          <button class="btn btn-sm btn-primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="fill-current w-3 h-3"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z"
+              >
+              </path>
+            </svg>
+          </button>
+        </div>
+        <span class="divider m-0 h-2"></span>
+        {children}
       </div>
     </div>
   );
--- routes/app.tsx
+++ routes/app.tsx
@@ -1,20 +1,39 @@
 import { defineRoute } from "$fresh/server.ts";
 import { type State } from "@/plugins/session.ts";
 import Card from "@/components/Card.tsx";
+import Table from "@/components/Table.tsx";
 
 export default defineRoute<State>((_req, _ctx) => {
   return (
     <div class="grid grid-cols-5 gap-4 min-h-full">
       <div class="gap-4 col-span-2 flex flex-col">
-        <Card height="h-2/4" />
-        <Card height="h-2/4" />
+        <Card classes="h-2/4" title="Fixed expenses">
+          <div>
+            Oi
+          </div>
+        </Card>
+        <Card classes="h-2/4" title="Over time expenses">
+          <div>
+            Oi
+          </div>
+        </Card>
       </div>
       <div class="gap-4 col-span-2 flex flex-col">
-        <Card height="h-full" />
+        <Card classes="h-no-nav" title="Current month expenses">
+          <Table />
+        </Card>
       </div>
       <div class="gap-4 col-span-1 min-h-full flex flex-col">
-        <Card height="h-2/4" />
-        <Card height="h-2/4" />
+        <Card classes="h-2/4" title="Total income">
+          <div>
+            Oi
+          </div>
+        </Card>
+        <Card classes="h-2/4" title="Total expenses">
+          <div>
+            Oi
+          </div>
+        </Card>
       </div>
     </div>
   );
