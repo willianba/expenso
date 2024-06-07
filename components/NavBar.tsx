34c7eafef679f35fb53411d9446a489ac7d18026
Willian Alves <wba.alves@gmail.com>
2024-06-07 00:22:07 -0300

    add simple menu to change months

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,6 +3,7 @@ import AuthButton from "@/islands/AuthButton.tsx";
 import { User } from "@/db/models/user.ts";
 import AppButton from "@/islands/AppButton.tsx";
 import ThemeController from "@/islands/ThemeController.tsx";
+import Menu from "@/islands/Menu.tsx";
 
 type NavBarProps = {
   sessionUser?: User;
@@ -15,6 +16,7 @@ export default function NavBar(props: NavBarProps) {
     <div class="navbar bg-base-100 fixed top-0 left-0 z-10 py-2 px-4">
       <div class="navbar-start">
         <BackButton />
+        <Menu />
       </div>
       <div class="navbar-center">
         <a class="text-xl font-semibold" href="/">Expenso</a>
---
+++ islands/Menu.tsx
@@ -0,0 +1,55 @@
+import { expenses } from "@/signals/expenses.ts";
+import { ExpenseWithoutUser } from "@/db/models/expense.ts";
+import { useState } from "preact/hooks";
+import { today } from "@/utils/date.ts";
+
+const months = [
+  { name: "January", number: 1 },
+  { name: "February", number: 2 },
+  { name: "March", number: 3 },
+  { name: "April", number: 4 },
+  { name: "May", number: 5 },
+  { name: "June", number: 6 },
+  { name: "July", number: 7 },
+  { name: "August", number: 8 },
+  { name: "September", number: 9 },
+  { name: "October", number: 10 },
+  { name: "November", number: 11 },
+  { name: "December", number: 12 },
+];
+
+export default function Menu() {
+  const [activeMonth, setActiveMonth] = useState(today().month);
+
+  const fetchExpenses = (month: number) => {
+    fetch(`/api/expenses/date?year=2024&month=${month}`).then(async (res) => {
+      const result = await res.json() as ExpenseWithoutUser[];
+      expenses.value = result;
+    });
+    setActiveMonth(month);
+  };
+
+  return (
+    <ul class="menu menu-horizontal bg-base-100 rounded-box">
+      <li>
+        <details>
+          <summary>
+            {months.find((m) => m.number === activeMonth)!.name}
+          </summary>
+          <ul>
+            {months.map((month) => (
+              <li>
+                <a
+                  class={activeMonth === month.number ? "active" : ""}
+                  onClick={() => fetchExpenses(month.number)}
+                >
+                  {month.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </details>
+      </li>
+    </ul>
+  );
+}
