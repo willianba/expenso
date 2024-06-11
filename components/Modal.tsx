52da3c7c824775cdb20d44917f03ca46b6701095
Willian Alves <wba.alves@gmail.com>
2024-06-10 22:06:00 -0300

    add confirmation modal on deleting expense

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -54,3 +54,37 @@ export function IncomeModal(props: IncomeModalProps) {
     </Modal>
   );
 }
+
+type ConfirmationModalProps = Omit<ModalProps, "children"> & {
+  onConfirm: () => void;
+  title: string;
+  message: string;
+};
+
+export function ConfirmationModal(props: ConfirmationModalProps) {
+  const { id, closeModal, title, message, onConfirm } = props;
+
+  return (
+    <Modal id={id} closeModal={closeModal}>
+      <h3 class="font-bold text-lg">{title}</h3>
+      <p class="py-4">{message}</p>
+      <div class="modal-action">
+        <form method="dialog" class="flex gap-1">
+          <button
+            class="btn btn-secondary"
+            onClick={closeModal}
+          >
+            Cancel
+          </button>
+          <button
+            class="btn btn-primary"
+            onClick={onConfirm}
+          >
+            Confirm
+          </button>
+        </form>
+      </div>
+    </Modal>
+  );
+}
+
--- islands/tables/ExpenseOptionsButton.tsx
+++ islands/tables/ExpenseOptionsButton.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "preact/hooks";
 import { expenses } from "@/signals/expenses.ts";
 import { ExpenseWithoutUser } from "@/db/models/expense.ts";
+import useModal from "@/islands/hooks/useModal.tsx";
+import { ConfirmationModal } from "@/components/Modal.tsx";
 
 type ExpenseOptionButtonProps = {
   expenseId: string;
@@ -9,6 +11,7 @@ type ExpenseOptionButtonProps = {
 export default function ExpenseOptionButton(props: ExpenseOptionButtonProps) {
   const { expenseId } = props;
 
+  const { modalId, openModal, closeModal } = useModal();
   const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
 
@@ -97,7 +100,7 @@ export default function ExpenseOptionButton(props: ExpenseOptionButtonProps) {
               <button
                 class="flex px-3 py-2 text-sm"
                 role="menuitem"
-                onClick={deleteExpense}
+                onClick={openModal}
               >
                 <svg
                   class="w-5 h-5 mr-1 text-red-400"
@@ -118,6 +121,13 @@ export default function ExpenseOptionButton(props: ExpenseOptionButtonProps) {
           </ul>
         </div>
       )}
+      <ConfirmationModal
+        id={modalId}
+        closeModal={closeModal}
+        title="Delete expense"
+        message="Are you sure you want to delete this expense?"
+        onConfirm={deleteExpense}
+      />
     </div>
   );
 }
