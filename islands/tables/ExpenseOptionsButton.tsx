3a125adb0e00e9d58484fc5acba838f671656f40
Willian Alves <wba.alves@gmail.com>
2024-06-10 21:03:02 -0300

    fix tiny visual border

--- islands/tables/ExpenseOptionsButton.tsx
+++ islands/tables/ExpenseOptionsButton.tsx
@@ -70,7 +70,7 @@ export default function ExpenseOptionButton(props: ExpenseOptionButtonProps) {
         </svg>
       </button>
       {isOpen && (
-        <div class="absolute right-0 mt-2 overflow-hidden shadow-md z-10">
+        <div class="absolute right-0 mt-2 overflow-hidden shadow-md rounded-box z-10">
           <ul class="menu bg-base-200 rounded-box gap-1">
             <li>
               <button
