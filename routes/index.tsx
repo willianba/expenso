038f5601911f3baa32804ab5d435fe9ba8b1fa73
Willian Alves <wba.alves@gmail.com>
2024-06-15 14:49:57 -0300

    add h2 as temporary msg

--- routes/index.tsx
+++ routes/index.tsx
@@ -11,7 +11,8 @@ export default defineRoute<State>((_req, _ctx) => {
         height="128"
         alt="the Fresh logo: a sliced lemon dripping with juice"
       />
-      <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+      <h1 class="text-4xl font-bold">Welcome to Expenso</h1>
+      <h2>This is a default page and logo, but the app works!</h2>
     </div>
   );
 });
