ac05f755b0614e554bd60dda7104c94bbeb4326f
Willian Alves <wba.alves@gmail.com>
2025-09-21 21:55:20 -0300

    migrate to fresh v2 beta

--- main.ts
+++ main.ts
@@ -1,18 +1,8 @@
-import { App, fsRoutes, staticFiles } from "fresh";
-
 import "@std/dotenv/load";
+import { App, staticFiles } from "fresh";
 
-export const app = new App();
-
-app.use(staticFiles());
-
-// Enable file-system based routing
-await fsRoutes(app, {
-  loadIsland: (path) => import(`./islands/${path}`),
-  loadRoute: (path) => import(`./routes/${path}`),
-});
-
-// If this module is called directly, start the server
-if (import.meta.main) {
-  await app.listen();
-}
+export const app = new App()
+  // Add static file serving middleware
+  .use(staticFiles())
+  // Enable file-system based routing
+  .fsRoutes();
